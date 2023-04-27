f2185ec529e1168dc369f7ead0ef8a5ed4dda2aa
MOLarin <9misha97@mail.ru>
2023-04-27 12:12:13 +0300

    Добавил приветствие людям

--- Core/src/main/java/academy/mow/helloWorld/Main.java
+++ Core/src/main/java/academy/mow/helloWorld/Main.java
@@ -3,5 +3,6 @@ package academy.mow.helloWorld;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello people!");
     }
 }
